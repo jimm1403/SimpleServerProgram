0437f1f3a4ec03ff8c57c704806b44e5e553446f
jimmi <jimmi@DESKTOP-2OTI0T5>
2017-08-23 12:00:06 +0200

    prøvet at få udpclient til at virke

--- ServerProgram/Program.cs
+++ ServerProgram/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
+using System.Text;
 
 namespace ServerProgram
 {
@@ -10,30 +11,84 @@ namespace ServerProgram
     {
         static void Main(string[] args)
         {
-            TcpListener serverSocket = new TcpListener(IPAddress.Any, 11000);
+            TcpListener serverSocket = new TcpListener(IPAddress.Parse("127.0.0.1"), 11000);
+            
             TcpClient clientSocket;
+            ServerProgram serverP = new ServerProgram();
 
             serverSocket.Start();
             Console.WriteLine("Server started");
-
+            Game serverGame = new Game();
+            
             while (true)
             {
                 clientSocket = serverSocket.AcceptTcpClient();
                 Console.WriteLine("Client: " + clientSocket.Client.RemoteEndPoint + " Connected!");
                 HandleClient client = new HandleClient();       //Hjemmelavet klasse
                 client.StartClient(clientSocket);
+                client.game = serverGame;
             }
         }
+    }
+    public class Game
+    {
+        UdpClient publisher = new UdpClient(11001);
+        //NetworkStream stream;
+        //StreamWriter pubSW;
+
+        int tries;
+        bool gameRun;
+        Random rNm = new Random();
+        int randomNr;
+        public bool GameRun { get { return gameRun; } set { gameRun = value; } }
+
+        public Game()
+        {
+            //pubSW = new StreamWriter(stream);
+            tries = 0;
+            gameRun = true;
+            randomNr = rNm.Next(1, 2);
+        }
+        public string Guess(int guess)
+        {            
+            tries++;
+            if (guess == randomNr)
+            {
+                Console.WriteLine("Caster til alle clients");
+                byte[] sdata = Encoding.ASCII.GetBytes("Spillet er vundet!");
+                publisher.Send(sdata, sdata.Length);
+                Reset();
+                return "Sådan verdensmand!";
+            }
+            else if(tries <= 8)
+            {
+                return "Gæt igen, " + tries + " forsøg brugt!";   
+            }
+            else
+            {
+                Reset();
+                
+                return "Du har tabt, ikke flere gæt!";
+            }
+        }
+        private void Reset()
+        {
+            tries = 0;
+            gameRun = false;
+            randomNr = rNm.Next(1, 21);
+        }
+
     }
     public class HandleClient
     {
         private TcpClient clientSocket; //Holder den client der er connected.
         private Thread thread;
-        
+        public Game game { get; set; }
+
         internal void StartClient(TcpClient client)
         {
             clientSocket = client;
-
+            
             IPEndPoint remoteIp = (IPEndPoint)clientSocket.Client.RemoteEndPoint;
             NetworkStream stream = new NetworkStream(clientSocket.Client);      //Kablet/røret fra server til client.
             StreamReader sr = new StreamReader(stream);                         //Står for at læse hvad der kommer fra clienten.
@@ -41,7 +96,6 @@ namespace ServerProgram
             
             thread = new Thread(() => ClientHandler(stream, sr, sw, remoteIp));
             thread.Start();
-            
         }
 
         internal void ClientHandler(NetworkStream NWS, StreamReader sr, StreamWriter sw, IPEndPoint remoteIp)
@@ -68,11 +122,9 @@ namespace ServerProgram
                         sw.WriteLine("The date today is " + DateTime.Today.Day + "-" +
                             DateTime.Today.Month + "-" + DateTime.Today.Year);
                     }
-
                     else if (message[0]=="game")
                     {
-                        Guessing(sw, sr);
-
+                        Game(sw, sr);
                     }
                     else if (message[0] == "add")
                     {
@@ -118,32 +170,18 @@ namespace ServerProgram
             }
             thread.Abort();
         }
-
-
-
-        public void Guessing(StreamWriter sw, StreamReader sr)
+        
+        public void Game(StreamWriter sw, StreamReader sr)
         {
-            Random rNm = new Random();
-            int number = rNm.Next(1, 10);
-
-            sw.WriteLine("Guess a number from 1 to 9");
+            sw.WriteLine("Guess a number from 1 to 20");
             sw.Flush();
+            game.GameRun = true;
 
-            while (true)
+            while (game.GameRun)
             {
-                
-                if (number == int.Parse(sr.ReadLine()))
-                {
-                    sw.WriteLine("Correct!");
-                    sw.Flush();
-                }
-                else
-                {
-                    sw.WriteLine("Guess again loser!");
-                    sw.Flush();
-                }
+                sw.WriteLine(game.Guess(int.Parse(sr.ReadLine())));
+                sw.Flush();    
             }
-            
-        }
+        }    
     }
 }
